ccb415ccba5a421d73a820fce88b564eb17a87a0
Ben Kimball <bkimball97@gmail.com>
2021-07-21 10:05:44 -0500

    pushing completed to chapter 13

--- employees/request.py
+++ employees/request.py
@@ -1,3 +1,8 @@
+import sqlite3
+import json
+
+from models import Employee
+
 EMPLOYEES = [
     {
         "name": "atokad",
@@ -12,57 +17,134 @@ EMPLOYEES = [
 ]
 
 def get_all_employees():
-    return EMPLOYEES
+    """Return a list of employees"""
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.db") as conn:
+        # Black box
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM employee e
+        """)
+
+        # Initialize an empty list to hold all employee representations
+        employees = []
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+        # Iterate list of data returned from database
+        for row in dataset:
+            # Create an employee instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Employee class imported above.
+            employee = Employee(row['id'], row['name'], row['address'],
+                                row['location_id'])
+            employees.append(employee.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(employees)
 
 
-# Function with a single parameter
+
+# SQL GET function
 def get_single_employee(id):
-    # Variable to hold the found employee, if it exists
-    requested_employee = None
+    """Return a single employee by Id"""
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    # Iterate the employeeS list above. Very similar to the
-    # for..of loops you used in JavaScript.
-    for employee in EMPLOYEES:
-        # Dictionaries in Python use [] notation to find a key
-        # instead of the dot notation that JavaScript used.
-        if employee["id"] == id:
-            requested_employee = employee
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM employee e
+        WHERE e.id = ?
+        """, ( id, ))
 
-    return requested_employee
+        # Load the single result into memory
+        data = db_cursor.fetchone()
+        # Create an employee instance from the current row
+        employee = Employee(data['id'], data['name'], data['address'],
+                                data['location_id'])
+
+        return json.dumps(employee.__dict__)
+
+
+
+# SQL GET with query parameters
+def get_employees_by_location(location_id):
+    """Returns a list of locations by Id"""
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the info you want
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM employee e
+        WHERE e.location_id = ?
+        """, ( location_id, ))
+
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['address'],
+                                row['location_id'])
+            employees.append(employee.__dict__)
+
+    return json.dumps(employees)
+
 
 def create_employee(employee):
-    # Get the id value of the last animal in the list
+    """Create a NEW employee
+    """
+    # Get the id value of the last location in the list
     max_id = EMPLOYEES[-1]["id"]
-
     # Add 1 to whatever that number is
     new_id = max_id + 1
-
-    # Add an `id` property to the animal dictionary
+    # Add an `id` property to the employee dictionary
     employee["id"] = new_id
-
     # Add the employee dictionary to the list
     EMPLOYEES.append(employee)
-
     # Return the dictionary with `id` property added
     return employee
 
+
+# SQL DELETE function
 def delete_employee(id):
-    # Initial -1 value for animal index, in case one isn't found
-    employee_index = -1
+    """Delete an employee by Id"""
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
 
-    # Iterate the ANIMALS list, but use enumerate() so that you
-    # can access the index value of each item
-    for index, employee in enumerate(EMPLOYEES):
-        if employee["id"] == id:
-            # Found the animal. Store the current index.
-            employee_index = index
+        db_cursor.execute("""
+        DELETE FROM employee
+        WHERE id = ?
+        """, ( id, ))
 
-    # If the animal was found, use pop(int) to remove it from list
-    if employee_index >= 0:
-        EMPLOYEES.pop(employee_index)
-        
+
+
 def update_employee(id, new_employee):
+    """Edit an employee by Id
+    """
+    # Iterate the EMPLOYEES list, but with enumerate() so that
+    # you can access the index value of each item
     for index, employee in enumerate(EMPLOYEES):
         if employee["id"] == id:
+            # Found the employee. Update the value.
             EMPLOYEES[index] = new_employee
             break
